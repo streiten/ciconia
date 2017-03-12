d5743ca87cd87c4b8edba549de8eb0cdc74d1a0f
Alex <alex@rainbow-unicorn.com>
2017-03-12 16:11:58 +0100

    API basics

--- index.js
+++ index.js
@@ -1,57 +1,101 @@
 var fs = require('fs');
-//var animal = require('./libs/animal.js');
-APPconfig = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+var winston = require('winston');
+var moment = require('moment');
+var express = require('express');
 
-var movebank = require('./libs/movebank.js');
+var app = express();
+var winston = require('winston');
+var http = require('http').Server(app);
 
-var mb = new movebank();
-mb.getStudies();
 
-setTimeout(test, 5000);
+/**
+ * The Webserving
+ */
 
-function test(){
-  for (var i = 0; i < mb.studies.length; i++) {
-    console.log(mb.studies[i].id + ' - ' + mb.studies[i].name);
+var httpport = 8080;
+
+app.use(express.static('public'));
+app.get('/',requestHandlerHome);
+
+function requestHandlerHome(request, response) {
+  response.sendFile( __dirname + '/views/index.html');
+  winston.log('info', 'Serving another request ' + request.hostname + ' to ' + request.ip );
+}
+
+http.listen(httpport, function(){
+  winston.log('info', 'Webserver started... on ' + httpport);
+});
+
+
+function Ciconia() {
+  
+  //var animal = require('./libs/animal.js');
+  APPconfig = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+  winston.level = 'debug';
+  winston.log('info', 'Cicona started...');
+
+  var movebank = require('./libs/movebank.js');
+
+  var mb = new movebank();
+  
+  mb.getStudyEvents(APPconfig.individuals[0].studyId,APPconfig.individuals[0].individualId,1);
+  setInterval(function(){
+    mb.getStudyEvents(APPconfig.individuals[0].studyId,APPconfig.individuals[0].individualId,1);
+  },15 * 60 * 1000);
+
+  // mb.getStudies();
+  // mb.getStudyDetails(APPconfig.studyID);
+  // mb.getStudyIndividuals(APPconfig.studyID);
+
+  mb.on('APIdataReady',dataReadyHandler);
+  function dataReadyHandler(type) {
+    switch(type) {
+      case 'studies':
+        //mb.downloadAllStudies();
+        winston.log('info', 'Event: studies');
+      break; 
+      case 'studyDetailsReady':
+        winston.log('info', 'Studies:');
+        for (var i = 0; i <  mb.studies.length; i++) {
+          winston.log('info', 'Name:',mb.studies[i].name,'- id:',mb.studies[i].id);
+        }
+      break; 
+      case 'studyEventsReady':
+        winston.log('info', 'Events:');
+
+        for (var j = mb.events.individuals.length - 1; j >= 0; j--) {
+          for (var i = 0; i <  mb.events.individuals[j].locations.length; i++) {
+            
+            var format = "llll";
+            var ts = moment(mb.events.individuals[j].locations[i].timestamp).format(format); 
+
+            winston.log('info', 
+              mb.events.individuals[j].individual_local_identifier,'- t:',
+              ts, ' - l:',
+              mb.events.individuals[j].locations[i].location_long,',',
+              mb.events.individuals[j].locations[i].location_lat)
+            ;
+          }
+        }
+
+      break; 
+      case 'studyIndividualsReady':
+        winston.log('info', 'Individuals:');
+
+        for (var i = 0; i <  mb.individuals.length; i++) {
+          winston.log('info', 'Name:',mb.individuals[i].local_identifier,'- id:',mb.individuals[i].id );
+        }
+
+      break; 
+      case 'studyDeploymentsReady':
+        winston.log('info', 'Event: studyDeploymentsReady');
+        debugger
+        winston.log('info', mb.response);
+
+      break; 
+    }
   }
-  console.log('Count:' +  mb.studies.length)
 }
-//console.log(APPconfig.animals[0].name);
 
-// getJSON(jsonUrl + "?callback=?", {
-//     study_id: study_id,
-//     individual_local_identifiers: individual_local_identifiers,
-//     max_events_per_individual : max_events_per_individual,
-//     // timestamp_start: timestamp_start, // !! enable to limit data display to a time range
-//     // timestamp_end: timestamp_end, // !! enable to limit data display to a time range
-//     sensor_type: "gps" // !! change if needed to specify the sensor type to display; options are gps, argos-doppler-shift, solar-geolocator, radio-transmitter, bird-ring, natural-mark
-// }, function (data0) {
-//     data = data0;
-//     for (i = 0; i < data.individuals.length; i++) {
-//         data.individuals[i].color = colors[i];
-//     }
-//     setBounds();
-//     createMarkers();
-//     createPolylines();
-//     //createPolylines2();
-
-//     startDate = null;
-//     endDate = null;
-//     for (i = 0; i < data.individuals.length; i++) {
-//         for (j = 0; j < data.individuals[i].locations.length; j++) {
-//             ts = data.individuals[i].locations[j].timestamp;
-//             if (startDate != null) {
-//                 startDate = Math.min(startDate, ts);
-//                 endDate = Math.max(endDate, ts);
-//             } else {
-//                 startDate = ts;
-//                 endDate = ts;
-//             }
-//         }
-//     }
-//   }
-// }
-
-
-function clearConsole() {
-  process.stdout.write('\x1B[2J\x1B[0f');
-}
+Ciconia();
--- libs/movebank.js
+++ libs/movebank.js
@@ -1,52 +1,123 @@
 var request = require('request');
 var parse = require('csv-parse');
+var winston = require('winston');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
 module.exports = MoveBank;
 
 function MoveBank(){
   this.user = APPconfig.user;
   this.password = APPconfig.password;
-  this.studies = [];
+  this.response = [];
+  this.jsonApiBaseURL = APPconfig.jsonApiBaseURL;
+  this.csvApiBaseURL = APPconfig.csvApiBaseURL;
 }
 
-MoveBank.prototype.getTestData = function(){
-  request(APPconfig.APITestURL, function (error, response, body) {
+util.inherits(MoveBank, EventEmitter);
+
+MoveBank.prototype.mbGet = function (endpoint){
+  request(endpoint, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log(body);
+      var options = { delimiter : ',' , columns: true };
+      parse(body, options, function(err, output){
+        if(err) throw err;
+        return output;
+        //winston.log('info', 'mbGet result: ' + output );
+    }.bind(this));
     }
-  });
+  }.bind(this)).auth(this.user, this.password, false);
 };
 
-MoveBank.prototype.getLastAnimalEvent = function (){
-
-};
-
-MoveBank.prototype.getAllAnimalEvents = function (){
-
-};
 
 MoveBank.prototype.getStudies = function() {
   console.log('Getting studies...');
   // Fields:
   //acknowledgements,bounding_box,citation,comments,grants_used,has_quota,i_am_owner,id,license_terms,location_description,main_location_lat,main_location_long,name,number_of_deployments,number_of_events,number_of_individuals,number_of_tags,principal_investigator_address,principal_investigator_email,principal_investigator_name,study_objective,study_type,suspend_license_terms,timestamp_end,timestamp_start,i_can_see_data,there_are_data_which_i_cannot_see
-  request('https://www.movebank.org/movebank/service/direct-read?entity_type=study', function (error, response, body) {
+  request(this.csvApiBaseURL + '?entity_type=study', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var options = { delimiter : ',' , columns: true };
-    //var that = this;
     parse(body, options, function(err, output){
       if(err) throw err;
 
       var counter = 0; 
       for (var study in output ) {
         if(study.i_can_see_data == 'false') {
-          // console.log(output[i].id + ' - ' +  output[i].name);
-          // delete output[i];
           output.splice(study,1);
         }
       }
-      this.studies = output;
+      this.response = output;
+      console.log('Fetched ' +  this.studies.length + ' studies...');
+      this.emit('APIdataReady','studies');
     }.bind(this));
   }
 }.bind(this)).auth(this.user, this.password, false);
 };
+
+MoveBank.prototype.downloadAllStudies = function() {
+  var i = 0;
+
+  var getStudyEvents = function (studiesCount){
+    console.log('Fetching ' + this.studies[i].id + ' - ' + this.studies[i].name);
+
+    if((i+1) == studiesCount) {
+      clearInterval(interval);
+    }
+    i++;
+  }.bind(this);
+  
+  var interval = setInterval(getStudyEvents,500,this.studies.length);
+
+};
+
+MoveBank.prototype.getStudyDetails = function($studyId){
+  
+  winston.log('info', 'Getting study details...');
+
+  request(this.csvApiBaseURL + '?entity_type=study&study_id=' + $studyId, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var options = { delimiter : ',' , columns: true };
+    parse(body, options, function(err, output){
+      if(err) throw err;
+      this.studies = output;
+      this.emit('APIdataReady','studyDetailsReady');
+    }.bind(this));
+  }
+}.bind(this)).auth(this.user, this.password, false);
+
+};
+
+MoveBank.prototype.getStudyEvents = function($studyId,$individualID,$count){
+  
+  if( typeof $count == undefined ) {
+    var $count = 10;
+  }
+
+  winston.log('info', 'Getting study events...');
+  // json-auth endpoint here
+  request(this.jsonApiBaseURL + '?study_id='+$studyId+'&individual_ids[]='+$individualID+'&max_events_per_individual='+$count+'&sensor_type=gps', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+      this.events = JSON.parse(body);
+      this.emit('APIdataReady','studyEventsReady');
+  } else {
+    throw error;
+  }
+}.bind(this)).auth(this.user, this.password, false);
+};
+
+MoveBank.prototype.getStudyIndividuals = function($studyId){
+  winston.log('info', 'Getting study individuals...');
+
+  request('https://www.movebank.org/movebank/service/direct-read?entity_type=individual&study_id=' + $studyId, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var options = { delimiter : ',' , columns: true };
+    parse(body, options, function(err, output){
+      if(err) throw err; 
+      this.individuals = output;
+      this.emit('APIdataReady','studyIndividualsReady');
+    }.bind(this));
+  }
+}.bind(this)).auth(this.user, this.password, false);
+};
+
 
