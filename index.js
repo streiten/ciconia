10d6ab61993f4851aa6b293a3d9715aec494a61e
Alex Tolar <>
2018-01-19 14:08:50 +0100

    static files prefix

--- index.js
+++ index.js
@@ -36,11 +36,16 @@ var httpport = 8888;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(compression());
+
 app.use(sass({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public')
 }));
-app.use(express.static('public'));
+
+// app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/',homeController.index);
 app.get('/status',statusController.index);
@@ -54,7 +59,6 @@ app.get('/story/:id/',storyController.index);
 app.get('/user/',userController.index);
 app.get('/user/confirm/:hash',userController.confirm);
 app.get('/user/unsubscribe/:hash',userController.unsubscribe);
-
 
 // Websockets
 io.on('connection', function (socket) {
@@ -81,7 +85,6 @@ io.on('connection', function (socket) {
 http.listen(httpport, function(){
   winston.log('info', 'Webserver listening on: ' + httpport);
 });
-
 
 function Ciconia() {
   winston.level = 'debug';
